ba31dd98525f3db5e98f8e74232de3997db983bf
kuo220 <k0978337296@gmail.com>
2024-09-14 18:19:30 +0800

    add get_prev_date_price_chg() in utils/finance.py

--- utils/finance.py
+++ utils/finance.py
@@ -1,6 +1,7 @@
 import numpy as np
 import datetime
 import shioaji as sj
+from utils.time import TimeTool
 
 
 class Stock:
@@ -16,3 +17,18 @@ class Stock:
             last_cnt=1
         )
         return tick.close[0] if len(tick.close) != 0 else np.nan
+    
+
+    @staticmethod
+    def get_prev_date_price_chg(api: sj.Shioaji, stock_id: str, date: datetime.date) -> float:
+        """ 取得指定股票在指定日期的前一個交易日的漲跌幅 """
+        
+        # 取得前一個交易日的日期
+        last_trading_date = TimeTool.get_last_trading_date(api, date)
+        last_two_trading_date = TimeTool.get_last_trading_date(api, last_trading_date)
+        
+        # 計算前一個交易日股票的漲幅
+        prev_close_price = Stock.get_close_price(api, stock_id, last_trading_date)
+        prev_two_close_price = Stock.get_close_price(api, stock_id, last_two_trading_date)
+        
+        return round((prev_close_price / prev_two_close_price - 1) * 100, 2)
