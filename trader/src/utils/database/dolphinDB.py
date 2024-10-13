faf052e407bff071f355b797db828dc0d188b89a
Kuo Fu Yu <k0978337296@gmail.com>
2024-10-13 17:47:31 +0800

    add new func & refactor create_dolphinDB func in dolphinDB.py

--- trader/src/utils/database/dolphinDB.py
+++ trader/src/utils/database/dolphinDB.py
@@ -1,3 +1,5 @@
+import os
+from pathlib import Path
 import dolphindb as ddb
 
 
@@ -9,67 +11,65 @@ class DolphinDB:
         self.table_name = table_name
         
         self.session = ddb.session()
-        self.session.connect("localhost", 8848, "admin", "123456")        
+        self.session.connect("localhost", 8848, "admin", "123456")  
         
         if (self.session.existsDatabase(db_path)):
-            print("Database exist")
+            print("Database exists!")
         else:
-            print("Database doesn't exist")
+            print("Database doesn't exist!")
         
             
     @staticmethod
-    def create_dolphingDB(db_name: str, table_name: str):
+    def create_dolphinDB(db_name: str, table_name: str):
         """ 創建 dolphinDB """
     
         session = ddb.session()
         session.connect("localhost", 8848, "admin", "123456")
         
-        start_time = '2020.01.01'
+        start_time = '2020.03.01'
         end_time = '2030.12.31'
         
-        script = f"""
-        if (!existsDatabase("dfs://{db_name}")) {{
-           create database "dfs://{db_name}"
-           partitioned by VALUE({start_time}..{end_time}), HASH([SYMBOL, 25]) 
-           engine='TSDB'
-        }}
-        
-        try {{
+        if session.existsDatabase(f"dfs://{db_name}"):
+            print("Database exists!")
+        else:
+            print("Database doesn't exist!\nCreating a database...")
+            script = f"""
+            create database "dfs://{db_name}"
+            partitioned by VALUE({start_time}..{end_time}), HASH([SYMBOL, 25]) 
+            engine='TSDB'
             create table "dfs://{db_name}"."{table_name}"(
-            stock_id SYMBOL
-            time NANOTIMESTAMP
-            close FLOAT
-            volume INT
-            bid_price FLOAT
-            bid_volume INT
-            ask_price FLOAT
-            ask_volume INT
-            tick_type INT
+                stock_id SYMBOL
+                time NANOTIMESTAMP
+                close FLOAT
+                volume INT
+                bid_price FLOAT
+                bid_volume INT
+                ask_price FLOAT
+                ask_volume INT
+                tick_type INT
             )
             partitioned by time, stock_id,
             sortColumns=[`stock_id, `time],
             keepDuplicates=ALL
-        }} catch (ex) {{
-            dropDatabase("dfs://{db_name}")
-            throw ex
-        }}
-        """
-        
-        try:
-            session.run(script)
-            print("dolphinDB create successfully!")
-        except Exception as e:
-            print(f"dolphinDB create unsuccessfully!\n{e}")
+            """
+            try:
+                session.run(script)
+                if session.existsDatabase(f"dfs://{db_name}"):
+                    print("dolphinDB create successfully!")
+                else:
+                    print("dolphinDB create unsuccessfully!")
+            except Exception as e:
+                print(f"dolphinDB create unsuccessfully!\n{e}")
             
-    
+       
     def add_csv_to_dolphinDB(self, csv_path: str):
-        """ 將 .csv 檔案存入創建好的 database """
+        """ 將單個 .csv 檔案存入創建好的 database """
         
         script = f"""
         db = database("{self.db_path}")
         schemaTable = table(
-            ["stock_id","time","close","volume","bid_price","bid_volume","ask_price","ask_volume","tick_type"] as columnName,
-            ["SYMBOL","NANOTIMESTAMP","FLOAT","INT","FLOAT","INT","FLOAT","INT","INT"] as columnType
+            ["stock_id", "time", "close", "volume", "bid_price", "bid_volume", "ask_price", "ask_volume", "tick_type"] as columnName,
+            ["SYMBOL", "NANOTIMESTAMP", "FLOAT", "INT", "FLOAT", "INT", "FLOAT", "INT", "INT"] as columnType
         )
         loadTextEx(
             dbHandle=db,
@@ -81,7 +81,6 @@ class DolphinDB:
             containHeader=true
         )
         """
-
         try:
             self.session.run(script)
             print("The csv file successfully save into database and table!")
@@ -90,6 +89,45 @@ class DolphinDB:
             print(f"The csv file fail to save into database and table!\n{e}")    
     
 
+    def add_all_csv_to_dolphinDB(self, dir_path: str):
+        """ 將多個 .csv 檔案存入建好的 database """
+        
+        # read all csv files in dir_path (.replace is for windows os)
+        csv_files = [str(dir_path / csv).replace('\\', '/') for csv in os.listdir(dir_path)]
+        print(f"* Total csv files: {len(csv_files)}")
+        
+        script = f""" 
+        db = database("{self.db_path}")
+        schemaTable = table(
+            ["stock_id", "time", "close", "volume", "bid_price", "bid_volume", "ask_price", "ask_volume", "tick_type"] as columnName,
+            ["SYMBOL", "NANOTIMESTAMP", "FLOAT", "INT", "FLOAT", "INT", "FLOAT", "INT", "INT"] as columnType
+        )
+        
+        total_csv = {len(csv_files)}
+        csv_cnt = 0
+        
+        for (csv_path in {csv_files}) {{
+            loadTextEx(
+                dbHandle=db,
+                tableName="{self.table_name}",
+                partitionColumns=["time", "stock_id"],
+                filename=csv_path,
+                delimiter=",",
+                schema=schemaTable,
+                containHeader=true
+            )
+            csv_cnt += 1
+            print("* Status: " + string(csv_cnt) + "/" + string(total_csv))
+        }}
+        """
+        try:
+            self.session.run(script)
+            print("All csv files successfully save into database and table!")
+
+        except Exception as e:
+            print(f"All csv files fail to save into database and table!\n{e}")
+
+        
     @staticmethod
     def delete_dolphinDB(db_path: str):
         """ 刪除資料庫 """
