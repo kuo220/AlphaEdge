0769be56847eeb6f1cdea10e0b39dfd7818bf498
Kuo Fu Yu <k0978337296@gmail.com>
2025-02-11 17:18:23 +0800

    add new API in Chip API

--- trader/utils/data.py
+++ trader/utils/data.py
@@ -31,6 +31,41 @@ class Chip:
         """
         df = pd.read_sql_query(query, self.conn)
         return df
+    
+    
+    def get_stock_chip(self, stock_id: str, start_date: datetime.date, end_date: datetime.date) -> pd.DataFrame:
+        """ 取得個股三大法人盤後籌碼資料 """
+        
+        if start_date > end_date:
+            return pd.DataFrame()
+        
+        query = f""" 
+        SELECT * FROM {self.table_name} WHERE 證券代號 = '{stock_id}' AND 日期 BETWEEN '{start_date}' AND '{end_date}'
+        """
+        df = pd.read_sql_query(query, self.conn)
+        return df
+
+    
+    def get_net_chip(self, start_date: datetime.date, end_date: datetime.date) -> pd.DataFrame:
+        """ 取得三大法人盤後籌碼 """
+        
+        if start_date > end_date:
+            return pd.DataFrame()
+        
+        df = self.get(start_date, end_date)
+        df = df.loc[:, ('日期', '證券代號', '證券名稱', '外資買賣超股數', '投信買賣超股數', '自營商買賣超股數')]
+        return df
+        
+    
+    def get_stock_net_chip(self, stock_id: str, start_date: datetime.date, end_date: datetime.date) -> pd.DataFrame:
+        """ 取得個股三大法人盤後籌碼 """
+        
+        if start_date > end_date:
+            return pd.DataFrame()
+        
+        df = self.get_stock_chip(stock_id, start_date, end_date)
+        df = df.loc[:, ('日期', '證券代號', '證券名稱', '外資買賣超股數', '投信買賣超股數', '自營商買賣超股數')]
+        return df
 
 
 class Tick:
