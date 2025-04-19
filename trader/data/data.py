9512a42468b0249562809bf89d25cb84ef38452b
Kuo Fu Yu <k0978337296@gmail.com>
2025-04-19 22:23:14 +0800

    delete crawler.py and data.py in utils

---
+++ trader/data/data.py
@@ -0,0 +1,27 @@
+import sqlite3
+import os
+import pandas as pd
+import datetime
+from pathlib import Path
+try:
+    import dolphindb as ddb
+except ModuleNotFoundError:
+    print("Warning: dolphindb module is not installed.")
+from .chip import Chip
+from .tick import Tick
+from .qx_data import QXData
+    
+
+class Data:
+    """ 股市資料 API """
+    
+    def __init__(self):
+        self.chip = Chip()
+        
+        """ Ensure dolphinDB server has been opened """
+        try:
+            self.tick = Tick()
+        except Exception as e:
+            print(e)
+            
+        self.quantx = QXData()
