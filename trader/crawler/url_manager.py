544a11107d210bdb5fefd7a132e4451c64952f74
kuo220 <k0978337296@gmail.com>
2025-06-22 00:58:32 +0800

    udpate URLManager's constants

--- trader/crawler/url_manager.py
+++ trader/crawler/url_manager.py
@@ -5,7 +5,7 @@ class URLManager:
     """ URL Manager """
     
     # 取得上市公司代號
-    twse_code_url: str = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=1&issuetype=1&industry_code=&Page=1&chklike=Y"
+    TWSE_CODE_URL: str = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=1&issuetype=1&industry_code=&Page=1&chklike=Y"
     
     # 取得上櫃公司代號
-    tpex_code_url: str = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=2&issuetype=4&industry_code=&Page=1&chklike=Y"
+    TPEX_CODE_URL: str = "https://isin.twse.com.tw/isin/class_main.jsp?owncode=&stockname=&isincode=&market=2&issuetype=4&industry_code=&Page=1&chklike=Y"
