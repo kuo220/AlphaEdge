46a447e132a5ccb229cdf6690a43f67e4ac45d8f
Kuo Fu Yu <k0978337296@gmail.com>
2025-07-01 16:03:29 +0800

    Update crawler/__init__.py

--- trader/crawler/__init__.py
+++ trader/crawler/__init__.py
@@ -8,4 +8,7 @@ from .utils.crawler_tools import CrawlerTools
 from .utils.url_manager import URLManager
 
 from .handlers.stock_chip_handler import StockChipHandler
+from .handlers.stock_tick_handler import StockTickHandler
+
 from .managers.stock_chip_manager import StockChipManager
+from .managers.stock_tick_manager import StockTickManager
