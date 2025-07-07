23086fd4d31d10623409be0c29da23578cce8250
Kuo Fu Yu <k0978337296@gmail.com>
2025-07-07 20:20:03 +0800

    1. Rename financial_report_crawler -> financial_statement_crawler 2. Add several methods in FinancialStatementCrawler

--- trader/pipeline/refactor_crawler/financial_statement_crawler.py
+++ trader/pipeline/refactor_crawler/financial_statement_crawler.py
@@ -1,68 +1,84 @@
+import datetime
 import os
+import pickle
+import random
+import re
 import shutil
+import sqlite3
+import sys
+import time
+import urllib.request
+import warnings
 import zipfile
-import logging as L
-from datetime import date
-from financials_api.crawlers.data_crawler import DataCrawler
-from financials_api.crawlers.crawler_helpers import FiscalDate
-from financials_api.crawlers.crawler_helpers import download_url, to_fiscal_date
+from io import StringIO
+from pathlib import Path
+from typing import List, Dict, Optional, Any
 
-class FinancialStatementCrawler(DataCrawler):
+import ipywidgets as widgets
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from dateutil.relativedelta import relativedelta
+from dateutil.rrule import DAILY, MONTHLY, rrule
+from fake_useragent import UserAgent
+from IPython.display import display
+from requests.exceptions import ConnectionError, ReadTimeout
+from tqdm import tqdm, tnrange, tqdm_notebook
+
+from trader.pipeline.crawlers.base import BaseCrawler
+from trader.pipeline.utils import URLManager
+from trader.pipeline.utils.crawler_utils import CrawlerUtils
+from trader.config import (
+    CRAWLER_DOWNLOADS_PATH,
+    FINANCIAL_REPORT_PATH,
+    QUANTX_DB_PATH,
+    CERTS_FILE_PATH
+)
+
+
+class FinancialStatementCrawler(BaseCrawler):
+    """ Crawler for quarterly financial reports """
+    """
+    目前公開資訊觀測站（mopsov.twse.com）提供的財務報表格式有更改
+    1. 舊制：2013 ~ 2018
+    2. 新制：2019 ~ present
+    """
+
     def __init__(self):
         super().__init__()
-        root_dir = os.getcwd()
-        self.temp_file_path = os.path.join(root_dir, "temp.zip")
-        self.local_html_dir = os.path.join(root_dir, "data", "financial_statement")
 
-    def crawl(self, date: date):
-        fiscal_date = to_fiscal_date(date)
-        self.download_from_mops(fiscal_date)
-        self.unzip_and_filter_htmls(fiscal_date)
+        self.payload: Dict[str] = {}
+        self.fr_dir: Path = FINANCIAL_REPORT_PATH
 
-    def download_from_mops(self, fiscal_date: FiscalDate):
-        """ download data from Market Observation Post System """
-        year, season = fiscal_date.year, fiscal_date.quarter
-        assert year >= 2019
-        url = (
-            f"https://mopsov.twse.com.tw/server-java/FileDownLoad?step=9&fileName="
-            f"tifrs-{year}Q{season}.zip&filePath=/home/html/nas/ifrs/{year}/"
-        )
-        L.info(f'downloading from {url} to {self.temp_file_path}')
-        download_url(url, self.temp_file_path)
-        L.info('finish download')
 
-    def unzip_and_filter_htmls(self, fiscal_date: FiscalDate):
-        path = os.path.join(self.local_html_dir, f"{fiscal_date.year}{fiscal_date.quarter}")
+    def crawl(self, *args, **kwargs) -> None:
+        """ Crawl Financial Report (Include 4 reports) """
+        pass
 
-        if os.path.isdir(path):
-            shutil.rmtree(path)
 
-        L.info('create new dir')
+    def setup(self, *args, **kwargs):
+        """ Set Up the Config of Crawler """
 
-        zipfiles = zipfile.ZipFile(open(self.temp_file_path, 'rb'))
-        zipfiles.extractall(path=path)
+        # Create Downloads Directory For Financial Reports
+        self.fr_dir.mkdir(parents=True, exist_ok=True)
 
-        L.info('extract all files')
 
-        fnames = [f for f in os.listdir(path) if f[-5:] == '.html']
-        fnames = sorted(fnames)
+    def crawl_balance_sheet(self):
+        """ Crawl Balance Sheet (資產負債表) """
+        pass
 
-        newfnames = [f.split("-")[5] + '.html' for f in fnames]
 
-        for old_fname, new_fname in zip(fnames, newfnames):
-            ticker_name = os.path.splitext(os.path.basename(new_fname))[0]
-            if len(ticker_name) != 4:
-                # [CHECK] bypass ticker such as 000538 for some reason
-                L.info(f'remove strange code id {new_fname}')
-                os.remove(os.path.join(path, old_fname))
-                continue
+    def crawl_comprehensive_income(self):
+        """ Crawl Statement of Comprehensive Income (綜合損益表) """
+        pass
 
-            if not os.path.exists(os.path.join(path, new_fname)):
-                os.rename(os.path.join(path, old_fname), os.path.join(path, new_fname))
-            else:
-                os.remove(os.path.join(path, old_fname))
 
-if __name__ == "__main__":
-    L.basicConfig(level=L.INFO)
-    crawler = FinancialStatementCrawler()
-    df = crawler.crawl(date=date(2025, 3, 17))
+    def crawl_cash_flow(self):
+        """ Crawl Cash Flow Statement (現金流量表) """
+        pass
+
+
+    def crawl_equity_changes(self):
+        """ Crawl Statement of Changes in Equity (權益變動表) """
+        pass
