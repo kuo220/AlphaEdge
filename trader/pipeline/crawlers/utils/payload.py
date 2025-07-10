fcf04e54a6665a1dd642910afd61c3aa83f5f76c
Kuo Fu Yu <k0978337296@gmail.com>
2025-07-10 17:37:18 +0800

    Add month attribute in Payload

--- trader/pipeline/crawlers/utils/payload.py
+++ trader/pipeline/crawlers/utils/payload.py
@@ -9,8 +9,10 @@ class Payload:
     firstin: Optional[str] = "1"                # default: 1
     step: Optional[str] = "1"                   # default: 1
     TYPEK: Optional[str] = None                 # {sii: 上市, otc: 上櫃, all: 全部, sii0: 國內上市, sii1: 國外上市, otc0: 國內上櫃, otc1: 國外上貴}
+
     co_id: Optional[str] = None                 # Stock code
     year: Optional[str] = None                  # ROC year
+    month: Optional[str] = None                 # Month (mm)
     season: Optional[str] = None                # Season
 
 
