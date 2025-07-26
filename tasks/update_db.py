60cad1a1063effb7aa1f23be4fa15a718e0d40c8
Kuo Fu Yu <k0978337296@gmail.com>
2025-07-26 11:33:30 +0800

    Refactor: Move non-sensitive config values and paths from .env to config.py

---
+++ tasks/update_db.py
@@ -0,0 +1,30 @@
+import sys
+from pathlib import Path
+import os
+import sqlite3
+
+try:
+    import dolphindb as ddb
+except ModuleNotFoundError:
+    print("Warning: dolphindb module is not installed.")
+
+import ipywidgets as widgets
+from IPython.display import display
+from tqdm import tqdm
+from tqdm import tnrange, tqdm_notebook
+from dateutil.rrule import rrule, DAILY, MONTHLY
+from dateutil.relativedelta import relativedelta
+
+from trader.utils import ShioajiAccount
+from trader.pipeline import StockChipCrawler, StockTickCrawler, QuantXCrawler
+from trader.config import QUANTX_DB_PATH
+
+
+"""
+This script is used to update the Chip, Tick, and QuantX databases all at once.
+"""
+
+# TODO: 設定各資料庫更新的日期區間
+
+if __name__ == "__main__":
+    pass
