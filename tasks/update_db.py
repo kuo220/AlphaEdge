b1c0be49077c40df99082d4a83dee53eeb7a3574
kuo220 <k0978337296@gmail.com>
2025-11-20 17:34:48 -0500

    Modify the get_update_time_config in update_db.py

--- tasks/update_db.py
+++ tasks/update_db.py
@@ -56,18 +56,18 @@ from trader.pipeline.utils import DataType
 
 - Usage Example:
     - 僅更新 tick：
-        python -m tasks.update_db.py --target tick
+        python -m tasks.update_db --target tick
 
     - 更新三大法人與收盤價：
-        python -m tasks.update_db.py --target chip price
+        python -m tasks.update_db --target chip price
 
     - 更新所有資料（不含 tick）：
-        python -m tasks.update_db.py --target no_tick
+        python -m tasks.update_db --target no_tick
         or
-        python -m tasks.update_db.py
+        python -m tasks.update_db
 
     - 更新所有資料（含 tick）：
-        python -m tasks.update_db.py --target all
+        python -m tasks.update_db --target all
 """
 
 
@@ -89,8 +89,7 @@ def parse_arguments() -> argparse.Namespace:
 def get_update_time_config() -> Dict[str, datetime.date | int]:
     return {
         "start_date": datetime.date(2013, 1, 1),
-        # "end_date": datetime.date.today(),
-        "end_date": datetime.date(2025, 9, 1),
+        "end_date": datetime.date.today(),
         "start_year": 2013,
         "end_year": 2025,
         "start_month": 1,
